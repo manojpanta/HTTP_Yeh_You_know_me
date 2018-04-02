3f73ee07ac1d5003f7e86a1d829dd6dae014cefe
Manoj Panta <manojpanta95@gmail.com>
2018-04-01 20:56:04 -0600

    working on iteration 3

---
+++ lib/word_search.rb
@@ -0,0 +1,23 @@
+class WordSearch
+
+  attr_reader :file
+
+  def initialize
+    @file = File.read('/usr/share/dict/words')
+  end
+
+  def known?(word)
+    file.split("\n").include?(word)
+  end
+
+  def feedback(word)
+    if known?(word)
+      "#{word} is a known word"
+    else
+      "#{word} is not a known word"
+    end
+
+
+  end
+
+end
