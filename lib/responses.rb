a7a1bb8590d999b79e9a2c01c3c4b4fad56756cb
Manoj Panta <manojpanta95@gmail.com>
2018-04-04 11:06:45 -0600

    just moved those resonses into response module

--- lib/responses.rb
+++ lib/responses.rb
@@ -18,4 +18,42 @@ module Response
      'content-type: text/html; charset=iso-8859-1',
      "content-length: #{output.length}\r\n\r\n"].join("\r\n")
   end
+
+  def output
+    puts 'Sending response.'
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers = headers(output)
+    client.puts headers
+    client.puts output
+    puts ['Wrote this response:', headers, output].join("\n")
+  end
+
+  def response
+    if path == "Path: /hello\n"
+      respond_to_hello(count)
+
+    elsif path == "Path: /datetime\n"
+      respond_to_datetime
+
+    elsif path == "Path: /shutdown\n"
+      stop_listening(count)
+
+    elsif path == "Path: /game\n" and verb == "Verb: GET\n"
+      @game.response
+
+    elsif path.include?('start_game') && verb == "Verb: POST\n"
+      'Good Luck!'
+
+    elsif path == "Path: /game\n" and verb == "Verb: POST\n"
+      guess =  @client.read(content_length).split("=")[1]
+      @game.take_guesses(guess)
+
+    elsif path.include?('word_search?')
+      word = path.split[1].split('?')[1].split('=')[1]
+      @word_search.feedback(word)
+
+    else
+      '<pre>' + verb + path + protocol + host + port + origin + accept + '<pre>'
+    end
+  end
 end
